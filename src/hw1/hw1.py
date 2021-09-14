f88a08b71044065550aafe28eea861a27e7df9ea
zizhazhu <zizhazhu@gmail.com>
2021-09-14 20:18:19 +0800

    add util

--- src/hw1/hw1.py
+++ src/hw1/hw1.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
-from util.util import set_rand_seed, get_device
+from util.util import set_rand_seed, get_device, plot_learning_curve, plot_pred
 
 
 class COVID19Dataset(Dataset):
