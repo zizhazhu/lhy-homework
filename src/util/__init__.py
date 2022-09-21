afa4169939dc41ffcb716d81fcf15c572fa3bb01
zizhazhu <zizhazhu@gmail.com>
2022-09-21 11:19:53 +0800

    update trainer

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -1,1 +1,2 @@
+import util.train
 from .util import get_device, set_rand_seed
--- src/util/train/trainer.py
+++ src/util/train/trainer.py
@@ -1,20 +1,33 @@
+import torch
 import torch.nn as nn
 from tqdm.auto import tqdm
 
 
 class Trainer:
-    def __init__(self, model, criterion, optimizer, device='cpu'):
+    def __init__(self, model, criterion, optimizer, device='cpu', writer=None, model_path=None):
         self._model = model
         self._device = device
         self._criterion = criterion
         self._optimizer = optimizer
+        self._writer = writer
+        self._model_path = model_path
 
-    def train_and_eval(self, train_loader, val_loader, n_epochs=1):
+    def train_and_eval(self, train_loader, val_loader, n_epochs=1, verbose=False):
+        best_acc = 0.0
         for epoch in range(n_epochs):
-            self.train(train_loader)
-            self.eval(val_loader)
+            train_acc_avg, train_loss_avg = self.train(train_loader)
+            if verbose:
+                print(f"[{epoch+1}/{n_epochs} Train Acc: {train_acc_avg} Loss: {train_loss_avg}")
+            valid_acc_avg, valid_loss_avg = self.eval(val_loader)
+            if verbose:
+                print(f"Valid Acc: {valid_acc_avg} Loss: {valid_loss_avg}")
+            if valid_acc_avg > best_acc:
+                torch.save(self._model.state_dict(), self._model_path)
+                best_acc = valid_acc_avg
 
-    def train(self, train_loader, n_epochs=1):
+    def train(self, train_loader, n_epochs=1, verbose=False):
+        train_acc_avg = 0.0
+        train_loss_avg = 0.0
         for epoch in range(n_epochs):
             self._model.train()
             train_loss = []
@@ -41,7 +54,40 @@ class Trainer:
             train_loss_avg = sum(train_loss) / len(train_loss)
             train_acc_avg = sum(train_acc) / len(train_acc)
 
-    def eval(self, val_loader):
-        self._model.eval()
-        pass
+            if verbose:
+                print(f"[{epoch+1}/{n_epochs} Train Acc: {train_acc_avg} Loss: {train_loss_avg}")
+            if self._writer:
+                self._writer.add_scalar('acc/train', train_acc_avg, epoch+1)
+                self._writer.add_scalar('loss/train', train_loss_avg, epoch+1)
+        return train_acc_avg, train_loss_avg
 
+    def eval(self, val_loader, n_epochs=1, verbose=False):
+        valid_loss_avg = 0.0
+        valid_acc_avg = 0.0
+        for epoch in range(n_epochs):
+            self._model.eval()
+            valid_loss = []
+            valid_acc = []
+
+            for batch in tqdm(val_loader):
+                features, labels = batch
+                features = features.to(self._device)
+                labels = labels.to(self._device)
+
+                with torch.no_grad():
+                    logits = self._model(features)
+                loss = self._criterion(logits, labels)
+
+                acc = (logits.argmax(dim=-1) == labels).float().mean()
+                valid_loss.append(loss.item())
+                valid_acc.append(acc)
+
+            valid_loss_avg = sum(valid_loss) / len(valid_loss)
+            valid_acc_avg = sum(valid_acc) / len(valid_acc)
+
+            if verbose:
+                print(f"[{epoch+1}/{n_epochs} Valid Acc: {valid_acc_avg} Loss: {valid_loss_avg}")
+            if self._writer:
+                self._writer.add_scalar('acc/valid', valid_acc_avg, epoch+1)
+                self._writer.add_scalar('loss/valid', valid_loss_avg, epoch+1)
+        return valid_acc_avg, valid_loss_avg
